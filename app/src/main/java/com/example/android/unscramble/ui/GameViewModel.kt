efa8e763894d9737b1c84130ff54dc9669158001
cbarrios <lalobarrios16@yahoo.com>
2022-08-30 17:50:18 -0400

    master: (extra) Fix for randomness

--- app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
@@ -34,7 +34,7 @@ class GameViewModel : ViewModel() {
 
     private fun pickRandomWordAndShuffle(): String {
         // Continue picking up a new random word until you get one that hasn't been used before
-        currentWord = allWords.random()
+        currentWord = allWords.shuffled().random()
         return if (usedWords.contains(currentWord)) {
             pickRandomWordAndShuffle()
         } else {
